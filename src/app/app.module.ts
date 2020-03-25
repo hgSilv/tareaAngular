459926b71acf20f97f2382a66f393768443eaa16
Héctor Gerardo Silva Ayala <39529919+hgSilv@users.noreply.github.com>
2020-03-25 00:36:16 -0600

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductoComponent } from './producto/producto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
